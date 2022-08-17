bdd48c273817a289a9d99c9dcb937665213cbd98
Carlos Morales <carlosmorales@Carloss-MacBook-Pro-2.local>
2022-08-16 17:05:44 -0700

    created layout for bubble chart

--- charts.js
+++ charts.js
@@ -95,16 +95,7 @@ function buildCharts(sample) {
 
 
 //Bubble charts
-// Create the buildCharts function.
-function buildCharts(sample) {
-  // Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
-    
-
-    // Deliverable 1 Step 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot(); 
-
-    // 1. Create the trace for the bubble chart.
+// 1. Create the trace for the bubble chart.
     var bubbleData = {
       x: otuIDs,
       y: sampleVals,
@@ -119,9 +110,10 @@ function buildCharts(sample) {
 
     // 2. Create the layout for the bubble chart.
     var bubbleLayout = {
-      
+      title: "Bacteria Cultures Per Sample",
+      xaxis: {title: "OTU ID"},
+      showlegend: false
     };
-
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot(); 
   });
